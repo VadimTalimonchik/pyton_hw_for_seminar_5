e321aaca097144dd0f396c1597ddf503d22b1898
VadimTalimonchik <tvadmins@gmail.com>
2023-02-25 20:50:04 +0300

    Задача 28

--- hw_28.py
+++ hw_28.py
@@ -3,4 +3,15 @@
 # 2 2
 # 4
 
-print('hello 28')
+print()
+a = int(input("Введите число A: "))
+b = int(input("Введите число B: "))
+print()
+
+def sum(a, b):
+    if b == 0:
+        return a
+    return sum(a + 1, b - 1)
+
+print('Сумма чисел', a, 'и', b, 'равна', sum(a, b))
+print()
